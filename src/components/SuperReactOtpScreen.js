59bddec1e354f9564df9ecdf6c3784dd90516c90
pg07codes <pg07codes@gmail.com>
2020-06-18 11:41:23 +0530

    ressponsive form area done, testing deployment

---
+++ src/components/SuperReactOtpScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper'
+import './SuperReactOtpScreen.css';
+
+function SuperReactOtpScreen() {
+  return (
+    <>
+      <Grid container justify="center">
+        <Grid item lg={3} md={4} sm={6} xs={9}>
+          <Paper id="login-otp">
+            
+          </Paper>
+        </Grid>
+      </Grid>
+
+    </>
+  );
+}
+
+export default SuperReactOtpScreen;
