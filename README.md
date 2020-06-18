249136edfd91d5a52f1ea4c3beb86572408b7ab3
pg07codes <pg07codes@gmail.com>
2020-06-18 18:54:31 +0530

    day over

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # super-react-otp-screen
+
+- a responsive react OTP login screen
+- color schemes could have been better ;)
+- tbh, i dont know why i made this. maybe it was too hot to go out for a walk. 
+
+![](sshot.gif)
--- src/components/SuperReactOtpScreen.js
+++ src/components/SuperReactOtpScreen.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper'
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem'
 import './SuperReactOtpScreen.css';
 
 function SuperReactOtpScreen() {
   return (
     <>
       <Grid container justify="center">
-        <Grid item lg={3} md={4} sm={6} xs={9}>
+        <Grid item lg={4} md={6} sm={8} xs={11}>
           <Paper id="login-otp">
+            <div style={{ textAlign: 'center' }}>
+              <img id="logo-img" src={process.env.PUBLIC_URL + '/logo.svg'} alt="Super React OTP" />
+            </div>
+            <Grid container justify="center" spacing={2}>
+              <Grid item xs={4}>
+                <select name="codes" id="codes">
+              
+                  {[1, 2, 3, 4, 5, 6, 7].map((o) => (
+                    <option key={o} value={o}>
+                      {o}
+                    </option>
+                  ))}
+                </select>
             
+              </Grid>
+              <Grid item xs={7}>
+                <input type="text" placeholder="Enter 10 digit Mobile Number" />
+              </Grid>
+
+              <Grid item xs={11}>
+
+                <a href="/" class="send-otp-btn">Get OTP</a>
+
+              </Grid>
+            </Grid>
           </Paper>
         </Grid>
       </Grid>
